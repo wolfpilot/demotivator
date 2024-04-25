6edf50d83bc935956eef04e8120a0151b7db4d1c
Razvan Negrea <rznart@gmail.com>
2024-04-25 16:33:19 +0200

    Add new middleware wrapper for Controller async calls

--- src/routes/quotesRoutes.ts
+++ src/routes/quotesRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import asyncWrapper from "@myrotvorets/express-async-middleware-wrapper"
 
 // Controllers
 import * as controller from "@controllers/quotesController"
@@ -15,17 +16,21 @@ import { validate } from "@utils/validationHelper"
 
 const router = express.Router()
 
-router.get("/", controller.list)
-router.post("/", validate({ body: quotesCreateSchema }), controller.create)
+router.get("/", asyncWrapper(controller.list))
+router.post(
+  "/",
+  validate({ body: quotesCreateSchema }),
+  asyncWrapper(controller.create)
+)
 router.get(
   "/:id",
   validate({ params: quotesGetByIdSchema }),
-  controller.getById
+  asyncWrapper(controller.getById)
 )
 router.delete(
   "/:id",
   validate({ params: quotesDeleteByIdSchema }),
-  controller.deleteById
+  asyncWrapper(controller.deleteById)
 )
 
 export default router
